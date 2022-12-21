0bdb9a92ad9a6205dbe714fc02f598ea00bc5a68
hetot <shaxzod.murtazayev@mail.ru>
2022-12-21 13:23:14 +0300

    try4

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,28 +1,21 @@
-delete from payment
-where student_id in (
+delete from payment where student_id in (
     select id from student where groupnumber > 4
     );
 
-delete from mark
-where student_id in (
+delete from mark where student_id in (
     select id from student where groupnumber > 4
     );
 
-delete from student
-where groupnumber > 4;
+delete from student where groupnumber > 4;
 
-delete from student
-where id in (
+delete from student where id in (
     select student_id from mark where mark < 4
     );
 
-delete from payment
-where type_id in (
+delete from payment where type_id in (
     select id from paymenttype where name = 'DAILY'
     );
 
-delete from paymenttype
-where name = 'DAILY';
+delete from paymenttype where name = 'DAILY';
 
-delete from mark
-where mark < 7;
+delete from mark where mark < 7;
